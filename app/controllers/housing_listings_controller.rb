714ba380c1c8d74f7dcf150dca5d3b326966a6e0
Raghu Betina <raghu@firstdraft.com>
2019-05-29 20:55:51 +0000

    Only housing_listing user can modify housing_listing

--- app/controllers/housing_listings_controller.rb
+++ app/controllers/housing_listings_controller.rb
@@ -1,4 +1,14 @@
 class HousingListingsController < ApplicationController
+  before_action :current_user_must_be_housing_listing_sublessor, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_housing_listing_sublessor
+    housing_listing = HousingListing.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == housing_listing.sublessor
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @housing_listings = HousingListing.all
 
