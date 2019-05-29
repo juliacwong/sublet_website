6eab0e2789d0642e3d7acb8101b4698dd6205017
Raghu Betina <raghu@firstdraft.com>
2019-05-29 20:55:32 +0000

    Whitelist columns in HousingPhoto for ActiveAdmin

--- app/admin/housing_photos.rb
+++ app/admin/housing_photos.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register HousingPhoto do
+
+ permit_params :photo_caption, :photo_owner_id, :housing_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
