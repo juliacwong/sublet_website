321829c6c53ed3d6e3ba7dd1a461c89c50d3628d
Raghu Betina <raghu@firstdraft.com>
2019-05-29 20:55:51 +0000

    Only housing_like user can modify housing_like

--- app/controllers/housing_likes_controller.rb
+++ app/controllers/housing_likes_controller.rb
@@ -1,4 +1,14 @@
 class HousingLikesController < ApplicationController
+  before_action :current_user_must_be_housing_like_sublessee, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_housing_like_sublessee
+    housing_like = HousingLike.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == housing_like.sublessee
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @housing_likes = HousingLike.all
 
