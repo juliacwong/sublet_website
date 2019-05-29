c4305ae567dbe4c99e9f8c50a7387ef3f1d1b73f
Raghu Betina <raghu@firstdraft.com>
2019-05-29 20:55:29 +0000

    Whitelist columns in HousingLike for ActiveAdmin

--- app/admin/housing_likes.rb
+++ app/admin/housing_likes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register HousingLike do
+
+ permit_params :sublessee_id, :housing_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
