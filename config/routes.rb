48cd4790da9fb6364dd962bf084bf81da25a91c9
Raghu Betina <raghu@firstdraft.com>
2019-05-29 20:55:12 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "housing_listings#index"
   # Routes for the Housing photo resource:
 
