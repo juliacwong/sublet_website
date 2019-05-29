ad86c922922a275a3de61d131481076437835372
Raghu Betina <raghu@firstdraft.com>
2019-05-29 20:55:25 +0000

    Whitelist columns in Inquiry for ActiveAdmin

--- app/admin/inquiries.rb
+++ app/admin/inquiries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Inquiry do
+
+ permit_params :sublessee_id, :sublessor_id, :housing_id, :inquiry_details
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
