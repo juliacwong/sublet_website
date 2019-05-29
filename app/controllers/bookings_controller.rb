0a6b24ead1f7e8903406e0f92200c0b4bc0e9e22
Raghu Betina <raghu@firstdraft.com>
2019-05-29 20:55:51 +0000

    Only booking user can modify booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,14 @@
 class BookingsController < ApplicationController
+  before_action :current_user_must_be_booking_sublessee, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_booking_sublessee
+    booking = Booking.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == booking.sublessee
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @bookings = Booking.all
 
